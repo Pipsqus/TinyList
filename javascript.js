18eba7e1039490b7cba85fc247c7de2c66f440a1
Pipsqus <andreaforconi@protonmail.com>
2020-08-28 17:40:03 +0200

    localStorage saves the list

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
 // Add a new task
-function AddNewTask() {
-    var GivenObjectTask = document.getElementById("NewTask");
+function AddNewTask(GivenObjectTask) {
     var NewTask = GivenObjectTask.value;
     var NewTask = NewTask.trimRight();
     var NewTask = " " + NewTask.trimStart();
@@ -10,37 +9,73 @@ function AddNewTask() {
         var Box = document.createElement("INPUT");
         Box.id = NewTask;
         Box.type = "checkbox";
+        Box.addEventListener("click", function(event) {
+            localStorage.setItem(NewTask, true)
+          }
+        );
 
         var Label = document.createElement("LABEL");
-        var TextNode = NewTask
-        var TextNode = document.createTextNode(TextNode)
+        var TextNode = NewTask;
+        var TextNode = document.createTextNode(TextNode);
         Label.htmlFor = NewTask;
-        Label.appendChild(TextNode)
+        Label.appendChild(TextNode);
 
         var paragraph = document.createElement("P");
         paragraph.appendChild(Box);
-        paragraph.appendChild(Label)
+        paragraph.appendChild(Label);
 
         document.getElementById("list_tasks").appendChild(paragraph);
         StoreTask(NewTask);
       }
     } else {
-      window.alert("The task should be at least 3 characters long!")
+      window.alert("The task should be at least 3 characters long!");
     }
 };
-// save in localStorage the NewTask just appended by the previous function
+// save in localStorage the NewTask and its "checkness" state
 function StoreTask(NewTask) {
-  console.log("temporary function")
+  var Checkness = document.getElementById(NewTask).checked;
+  localStorage.setItem(NewTask, Checkness);
 }
 
+// restore the tasks onload()
+function LoadStoredTasks() {
+  for (i = 0; i < localStorage.length; i++) {
+    var StoredTask = localStorage.key(i)
 
-// ONLOAD restore the previously added tasks
+    var Box = document.createElement("INPUT");
+    Box.id = StoredTask;
+    Box.type = "checkbox";
+    Box.addEventListener("click", function(event) {
+        localStorage.setItem(StoredTask, true);
+      }
+    );
 
-// eventListener for pressing Enter while writing on the input box
+    if (localStorage.getItem(StoredTask) == "true") {
+      Box.checked = true
+    };
+
+    var Label = document.createElement("LABEL");
+    var TextNode = StoredTask;
+    var TextNode = document.createTextNode(TextNode);
+    Label.htmlFor = StoredTask;
+    Label.appendChild(TextNode);
+
+    var paragraph = document.createElement("P");
+    paragraph.appendChild(Box);
+    paragraph.appendChild(Label);
+
+    document.getElementById("list_tasks").appendChild(paragraph);
+  };
+
+}
+
+// store the checked attribute of every NewTask
 
 
+// cycle through Salmy pictures as tasks get completed
 
-/*  - add eventListener to check when you press Enter, and then run the button
+
+/*  DONE- add eventListener to check when you press Enter, and then run the button
     - use Salmy to communicate that the NewTask is invalid instead of opening a popup
     - localStorage setting to keep the boxinos
     -

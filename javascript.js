9417c01daccab6102580da20f3a58e7240c4b278
Pipsqus <andreaforconi@protonmail.com>
2020-08-29 17:18:48 +0200

    Today and tomorrow now work; localStorage set for all tasks and checks

--- javascript.js
+++ javascript.js
@@ -3,69 +3,86 @@ function AddNewTask(GivenObjectTask) {
     var NewTask = GivenObjectTask.value;
     var NewTask = NewTask.trimRight();
     var NewTask = " " + NewTask.trimStart();
+    var ActiveDiv = document.getElementsByClassName("active")[0];
+
 
     if (NewTask.length > 3) {
-      if (!(document.getElementById(NewTask))) {
-        var Box = document.createElement("INPUT");
-        Box.id = NewTask;
-        Box.type = "checkbox";
-        Box.addEventListener("click", function(event) {
-            localStorage.setItem(NewTask, true)
-          }
-        );
+      if (!(localStorage.getItem(NewTask))) {
+        StoreTask(NewTask, ActiveDiv);
 
-        var Label = document.createElement("LABEL");
-        var TextNode = NewTask;
-        var TextNode = document.createTextNode(TextNode);
-        Label.htmlFor = NewTask;
-        Label.appendChild(TextNode);
+        var Box = CreateBoxElement(NewTask);
+        var Label = CreateLabelElement(NewTask);
+        var Paragraph = CreateParagraphElement(Box, Label);
 
-        var paragraph = document.createElement("P");
-        paragraph.appendChild(Box);
-        paragraph.appendChild(Label);
-
-        document.getElementById("list_tasks").appendChild(paragraph);
-        StoreTask(NewTask);
+        ActiveDiv.appendChild(Paragraph);
       }
     } else {
       window.alert("The task should be at least 3 characters long!");
     }
+    GivenObjectTask.value = "";
 };
+
+// Create a Box
+function CreateBoxElement(TaskName) {
+  var StoredTask = localStorage.getItem(TaskName).slice(0, 5);
+  var Box = document.createElement("INPUT");
+  Box.id = TaskName;
+  Box.type = "checkbox";
+  Box.addEventListener("click", function(event) {
+      localStorage.setItem(TaskName, (StoredTask + true))
+    }
+  );
+  return Box;
+}
+// Create a Label for the Box
+function CreateLabelElement(TaskName) {
+  var Label = document.createElement("LABEL");
+  var TextNode = TaskName;
+  var TextNode = document.createTextNode(TextNode);
+  Label.htmlFor = TaskName;
+  Label.appendChild(TextNode);
+  return Label
+}
+// Create a P element where to append Box and Label
+function CreateParagraphElement(Box, Label) {
+  var paragraph = document.createElement("P");
+  paragraph.appendChild(Box);
+  paragraph.appendChild(Label);
+  return paragraph
+}
+
 // save in localStorage the NewTask and its "checkness" state
-function StoreTask(NewTask) {
-  var Checkness = document.getElementById(NewTask).checked;
-  localStorage.setItem(NewTask, Checkness);
+function StoreTask(NewTask, ActiveDiv) {
+  var Checkness = "false";
+
+  if (toggleIterator%2 == 0) {
+      localStorage.setItem(NewTask, ("Today" + Checkness));
+  } else {
+      localStorage.setItem(NewTask, ("Tmrrw" + Checkness))
+  }
 }
 
 // restore the tasks onload()
 function LoadStoredTasks() {
   for (i = 0; i < localStorage.length; i++) {
     var StoredTask = localStorage.key(i)
+    var TestDate = localStorage.getItem(StoredTask).slice(0, 5);
+    if (TestDate == "Today") {
+      var ListTasks = document.getElementById("list_tasks")
+    } else {
+      var ListTasks = document.getElementById("list_tomorrow_tasks")
+    }
 
-    var Box = document.createElement("INPUT");
-    Box.id = StoredTask;
-    Box.type = "checkbox";
-    Box.addEventListener("click", function(event) {
-      var Id = this.id;
-      localStorage.setItem(Id, true);
-      }
-    );
+    var Box = CreateBoxElement(StoredTask);
+    var Label = CreateLabelElement(StoredTask)
+    var Paragraph = CreateParagraphElement(Box, Label)
 
-    if (localStorage.getItem(StoredTask) == "true") {
+    var TestCheckness = localStorage.getItem(StoredTask).slice(5, 9);
+    if (TestCheckness == "true") {
       Box.checked = true
     };
 
-    var Label = document.createElement("LABEL");
-    var TextNode = StoredTask;
-    var TextNode = document.createTextNode(TextNode);
-    Label.htmlFor = StoredTask;
-    Label.appendChild(TextNode);
-
-    var paragraph = document.createElement("P");
-    paragraph.appendChild(Box);
-    paragraph.appendChild(Label);
-
-    document.getElementById("list_tasks").appendChild(paragraph);
+    ListTasks.appendChild(Paragraph);
   };
 
 }
@@ -73,6 +90,7 @@ function LoadStoredTasks() {
 // Reset button
 function ResetList() {
   document.getElementById("list_tasks").innerHTML = "";
+  document.getElementById("list_tomorrow_tasks").innerHTML = "";
   localStorage.clear()
 }
 
